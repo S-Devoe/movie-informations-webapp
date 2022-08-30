37035f9677f5963077527f08d225469c486aa752
Lagbalu Lateef <chelseg14@gmail.com>
2022-08-30 18:06:43 +0100

    v2.0

--- components/molecules/Collections.jsx
+++ components/molecules/Collections.jsx
@@ -19,7 +19,7 @@ const Collections = ({
   const [data, setData] = useState([]);
   const [limitData, setLimitData] = useState([]);
 
-  useEffect(() => {
+  // useEffect(() => {
     const fetchTrending = async () => {
       try {
         const response = await fetch(
@@ -37,8 +37,8 @@ const Collections = ({
 
     fetchTrending();
 
-     
-  });
+    // eslint-disable-next-line 
+  // }, []);
 
   
 
